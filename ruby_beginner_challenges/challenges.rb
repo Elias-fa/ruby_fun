eb917ea115d5bcd17952627bc09cf95492dce3ee
Elias Alewel <eliasalewel82b@gmail.com>
2021-12-06 21:17:08 -0500

    some stuff

---
+++ ruby_beginner_challenges/challenges.rb
@@ -0,0 +1,55 @@
+#####///////#####
+
+# iterate through the array and count all values > 5 (answer should be a count of 4)
+
+# array =  [17, 7, 3, 6, 10, 1]
+
+# def greater_than_five(array)
+#   print array.select {|number| number > 5}.count
+# end
+
+# greater_than_five(array)
+
+#####///////#####
+
+# algorithm to find prime numbers, divided by self and 1. check if divisible by self
+# and numbers down to 1.
+
+# require 'prime'
+
+# def prime1?(num)
+#   return if num <= 1
+#   (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+# end
+
+# def prime2?(num)
+#   return false if num <= 1
+#   Math.sqrt(num).to_i.downto(2) {|i| return false if num % i == 0}
+#   true
+# end
+
+# def prime3?(num)
+#   Prime.prime?(num)
+# end
+
+# def prime4?(num)
+#   ('1' * num) !~ /^1?$|^(11+?)\1+$/
+# end
+
+# puts prime1?(3)
+# puts prime2?(13)
+# puts prime3?(100)
+# puts prime4?(75)
+
+#####///////#####
+require 'prime'
+array = [2, 3, 4, 6, 8]
+
+def prime_count(array)
+  array.select {|number| Prime.prime(number)}.count
+end
+
+prime_count(array)
+
+
+
