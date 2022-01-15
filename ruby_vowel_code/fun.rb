d85fbd4257ca20c5e57d1260bf21dd5bd0e548e2
Elias Alewel <eliasalewel82b@gmail.com>
2022-01-15 09:07:25 -0500

    practice

--- ruby_vowel_code/fun.rb
+++ ruby_vowel_code/fun.rb
@@ -1,36 +1,47 @@
 
-@vowels = ['a', 'e', 'i', 'o', 'u'] #@ symbol turned this into an instance variable so we can use this object outside the method used.
-sentence = "this has 4 vowels"
+# @vowels = ['a', 'e', 'i', 'o', 'u'] #@ symbol turned this into an instance variable so we can use this object outside the method used.
+# sentence = "this has 4 vowels"
 
 
 
-def vowel_count(ex_str)
-  print ex_str.split("").select { |l| @vowels.any?(l) }.count
+# def vowel_count(ex_str)
+#   print ex_str.split("").select { |l| @vowels.any?(l) }.count
+# end
+
+
+
+# vowel_count(sentence)
+
+# class Paul
+
+#   LOVE_PHRASE = "I LOVE PAUL"
+
+#   def i_love_paul(start, stop)
+#     start.downto(stop) {|love| puts paul_love(love)}
+#   end
+
+#   def paul_love(the_love)
+#     LOVE_PHRASE
+#   end
+
+# end
+
+# Paul.new.i_love_paul(99, 1)
+
+# 100.times do 
+#   puts "I LOVE PAUL"
+# end
+
+# def isogram(given_string)
+#   given_string.split.match?(given_string.split)
+# end
+
+my_string = 'Threet'
+
+def isogram(given_string)
+  downcase_string = given_string.downcase
+  downcase_string.split('').map { |i| downcase_string.match?(i * 2) }.any?(true)
 end
 
-
-
-vowel_count(sentence)
-
-class Paul
-
-  LOVE_PHRASE = "I LOVE PAUL"
-
-  def i_love_paul(start, stop)
-    start.downto(stop) {|love| puts paul_love(love)}
-  end
-
-  def paul_love(the_love)
-    LOVE_PHRASE
-  end
-
-end
-
-Paul.new.i_love_paul(99, 1)
-
-100.times do 
-  puts "I LOVE PAUL"
-end
-
-
-
+# puts isogram(my_string)
+puts my_string.downcase
